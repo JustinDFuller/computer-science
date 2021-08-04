b40ed75b89e9b0d244c2e4e57fe6ca27d3400ff6
Justin Fuller <justindanielfuller@gmail.com>
2021-08-04 15:22:46 -0400

    Create binary_tree.go

---
+++ go/data_structures/binary_tree.go
@@ -0,0 +1,26 @@
+# Binary Tree
+
+A tree data structure represents a hierarchical dataset, like a graph.
+
+Each node in the tree has a value, as well as links to other nodes in the tree. In a binary tree, each node has at most 2 children. These children are referred to as `left` and `right`.
+
+## Traversing a tree.
+
+There are many ways to iterate or traverse over the nodes in a tree.
+
+## Pre-Order Traversal
+
+Visit the root first, then recursively visit the left, then the right nodes in a tree.
+
+## In-Order Traversal
+
+Recursively visit the left, then the root, then the right nodes in a tree.
+
+For a binary search tree, we can retrieve data in sorted order with an in-order traversal.
+
+## Post-Order Traversal
+
+Visit the left nodes first, visit the right nodes second, visit the root node last.
+
+Deleting from a tree is always in post-order.
+
